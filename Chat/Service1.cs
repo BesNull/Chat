51d3a2153e107653a724c27690769f37bf9677ae
BesNull <67557818+BesNull@users.noreply.github.com>
2020-07-24 17:08:58 +0300

    Add files via upload

---
+++ Chat/Service1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Chat
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде и файле конфигурации.
+    public class Service1 : IService1
+    {
+        List<ServerUser> users = new List<ServerUser>();
+        int nextID = 1;
+
+        public int Connect(string name)
+        {
+            ServerUser user = new ServerUser()
+            {
+                ID = nextID,
+                Name = name,
+                operationContext = OperationContext.Current
+            };
+            nextID++;
+            SendMsg(": "+user.Name+" connected to chat!", 0);
+
+            users.Add(user);
+            return user.ID;
+        }
+
+        public void Disconnect(int id)
+        {
+            var user = users.FirstOrDefault(i => i.ID == id);
+            if (user!=null)
+            {
+                users.Remove(user);
+                SendMsg(": "+user.Name + " disconnected from chat!", 0);
+            }
+        }
+
+        public void SendMsg(string msg, int id)
+        {
+            foreach (var item in users)
+            {
+                string answer = DateTime.Now.ToShortTimeString();
+
+                var user = users.FirstOrDefault(i => i.ID == id);
+                if (user!=null)
+                {
+                    answer += ": " + user.Name + " ";
+                }
+
+                answer += msg;
+                item.operationContext.GetCallbackChannel<IServerChatCallback>().MsgCallback(answer);
+
+
+            }
+        }
+    }
+}
